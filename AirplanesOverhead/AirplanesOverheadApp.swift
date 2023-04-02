9f9ac3b5a3d6c2fa6e892a7de99de6911f1b43e6
John Koehl <jkoehl@gmail.com>
2023-04-02 09:45:11 -0400

    Initial Commit

---
+++ AirplanesOverhead/AirplanesOverheadApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirplanesOverheadApp.swift
+//  AirplanesOverhead
+//
+//  Created by John Koehl on 4/2/23.
+//
+
+import SwiftUI
+
+@main
+struct AirplanesOverheadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirplanesOverheadUITests/AirplanesOverheadUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AirplanesOverheadUITestsLaunchTests.swift
+//  AirplanesOverheadUITests
+//
+//  Created by John Koehl on 4/2/23.
+//
+
+import XCTest
+
+final class AirplanesOverheadUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
